1524f82ee0d4d1d5f7cda17b008389af2ee3fc78
bdbaraban <375@holbertonschool.com>
2019-04-16 03:18:14 +0000

    Simplify fix - set (*head)->prev->prev = (*head)->prev; to (*head)->prev->next = (*head)->next;

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,32 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp = *head;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
 	if (*head == NULL)
+	{
 		return (-1);
-
-	for (; index != 0; index--)
-	{
-		if (tmp == NULL)
-			return (-1);
-		tmp = tmp->next;
 	}
-
-	if (tmp == *head)
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
 	{
-		*head = tmp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
+		*head = (*head)->next;
+		p++;
 	}
-
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
 	else
 	{
-		tmp->prev->next = tmp->next;
-		if (tmp->next != NULL)
-			tmp->next->prev = tmp->prev;
+		(*head)->prev->next = (*head)->next;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
 	}
-
-	free(tmp);
 	return (1);
 }
